2438171c6584282de205fceb5ac7ec79f5a5bb66
rakanasd1977 <rakan.asd1977@gmail.com>
2026-08-25 09:57:21 -0700

    Delete server directory

--- server/src/db/migrations/015_rating_replies.ts
+++
@@ -1,6 +0,0 @@
-const { db, run, all, get } = require('../index');
-
-module.exports = {
-  name: "015_rating_replies",
-  up: ()=>{db.exec("ALTER TABLE provider_ratings ADD COLUMN reply TEXT;");db.exec("ALTER TABLE provider_ratings ADD COLUMN replied_at TEXT;")},
-};
--- server/src/db/migrations/029_free_shipping_setting.ts
+++
@@ -1,6 +0,0 @@
-const { db, run, all, get } = require('../index');
-
-module.exports = {
-  name: "029_free_shipping_setting",
-  up: ()=>{const exists=get("SELECT key FROM settings WHERE key = ?",["free_shipping_min"]);if(!exists){run("INSERT INTO settings (key, value, label) VALUES (?,?,?)",["free_shipping_min","50000","\u0627\u0644\u062D\u062F \u0627\u0644\u0623\u062F\u0646\u0649 \u0644\u0644\u0634\u062D\u0646 \u0627\u0644\u0645\u062C\u0627\u0646\u064A (\u062F\u064A\u0646\u0627\u0631)"])}},
-};
