776980181a198529f5e7d71bb77c12c076917883
rakanasd1977 <rakan.asd1977@gmail.com>
2026-09-05 07:47:12 -0700

    Delete admin-panel directory

--- admin-panel/src/push.ts
+++
@@ -1,5 +0,0 @@
-import { api } from './api';
-import { createPanelPush } from '@rafidain/shared/default-push';
-
-export const push = createPanelPush(api);
-export const { pushSupported, registerSW, enablePush, disablePush } = push;
