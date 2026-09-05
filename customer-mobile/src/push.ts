d07aa48475916c82c58de13707e59d669b1f3e57
rakanasd1977 <rakan.asd1977@gmail.com>
2026-09-05 07:49:32 -0700

    Delete customer-mobile directory

--- customer-mobile/src/push.ts
+++
@@ -1,6 +0,0 @@
-import { createPush } from '@rafidain/shared/push';
-import { api } from './api';
-
-const push = createPush({ api, swUrl: '/sw.js', unwrap: true });
-
-export const { enablePush, disablePush } = push;
